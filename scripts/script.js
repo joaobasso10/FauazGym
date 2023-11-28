7a2b91e850f601b23815b71845a630e52398f643
joaobasso10 <joaovictorribbasso@gmail.com>
2023-11-27 23:07:06 -0300

    add links to whatsapp

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const contatoHero = document.getElementById("contatoHero");
+const contatoNewsLetter = document.getElementById("contatoNewsLetter");
+
+contatoHero.addEventListener("click", ()=>{
+    window.location.href = "https://wa.me//5541998737256?text=Olá,%20gostaria%20de%20saber%20mais%20sobre%20os%20planos%20de%20companhamento"
+})
+contatoNewsLetter.addEventListener("click", ()=>{
+    window.location.href = "https://wa.me//5541998737256?text=Olá,%20gostaria%20de%20saber%20mais%20sobre%20os%20planos%20de%20companhamento"
+})
